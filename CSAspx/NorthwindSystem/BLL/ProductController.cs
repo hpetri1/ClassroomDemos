485b00322f9ed5a41156c98f75f5f5c08fb1e599
hpetri1 <39070807+hpetri1@users.noreply.github.com>
2018-07-23 18:54:50 -0600

    WebForms CRUD

--- CSAspx/NorthwindSystem/BLL/ProductController.cs
+++ CSAspx/NorthwindSystem/BLL/ProductController.cs
@@ -39,5 +39,76 @@ namespace NorthwindSystem.BLL
             }
         }
 
+        //July 23, 2018
+        //this method will add a new product to the SQL product table
+        //this method will do the Add via EntityFramework
+        //optionally, one can pass back the new IDENTITY value from the successful add
+        public int Products_Add(Product newproduct)
+        {
+            //start the Insert transaction
+            using (var context = new NorthwindContext())
+            {
+                //staging:
+                //stage the new record to the DbSet<T> for the object instance
+                //at this time, the record IS NOT physically on the database
+                context.Products.Add(newproduct);
+
+                //commit the record to the Database
+                //any entity validaytion is done at this time
+                //if this statement is NOT executed, the insert is NOT completed (Rollback)
+                //if this statement is executed BUT FAILS for some reson, the insert is NOT completed (Rollback)
+                //if this statement is executed AND is successful, then the insert has been physically placed
+                //  the record on the database. At this time you can retrieve the new IDENTITY value
+                context.SaveChanges();
+
+                //after the success of the SaveChanges() you can access your instance for the new IDENTITY value
+                return newproduct.ProductID; // this line is optional, don't have to return it if I don't want to => public int Products_Add(Product newproduct)
+                                             // would be: public void Products_Add(Product newproduct)
+            }
+        }
+
+        //this method updates the database
+        //this method returns the number of records affected on the database
+        public int Products_Update(Product item)
+        {
+            //start transaction
+            using (var context = new NorthwindContext())
+            {
+                //stage
+                context.Entry(item).State = System.Data.Entity.EntityState.Modified;
+
+                //commit
+                return context.SaveChanges();
+            }
+        }
+
+        //this method deletes a record from the database
+        //   or 
+        //this method logically flags a record to be deemed deleted from the database
+        //this method returns the number of records affected on the database
+        public int Products_Delete(int productid)
+        {
+            //start transaction
+            using (var context = new NorthwindContext())
+            {
+                ////PHYSICAL DELETE
+                //var existing = context.Products.Find(productid);
+                ////stage
+                //context.Products.Remove(existing);
+                ////commit
+                //return context.SaveChanges();
+
+                //LOGICAL DELETE (is really an Update)
+                var existing = context.Products.Find(productid);
+                //alter the data value on the record that will logically deem the record deleted
+                //You should NOT rely on the user to do this alternation on the web form
+                existing.Discontinued = true;
+                //stage
+                context.Entry(existing).State = System.Data.Entity.EntityState.Modified;
+                //commmit
+                return context.SaveChanges();
+            }
+        }
+
     }
 }
